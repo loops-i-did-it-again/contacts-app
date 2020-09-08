49beba8b048c1f8aea2a9ed9bde2912d256370f0
dani <dani.zaghian@gmail.com>
2020-09-08 13:09:57 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new(first_name: "Jimmy", last_name: "Wilson", email: "jimmy@gmail.com", phone_number: "999-9999")
+contact.save
+
+contact = Contact.new(first_name: "Winston", last_name: "Barbe", email: "winston@gmail.com", phone_number: "888-8888")
+contact.save
+
+# Contact.create(first_name: "Jimmy", last_name: "Wilson", email: "jimmy@gmail.com", phone_number: "999-9999")
